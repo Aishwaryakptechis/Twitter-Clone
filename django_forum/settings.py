26fe07aaf008346b1c5095e1b408f155390e8da3
aishwaryakptechis <aishwaryakp@techis.io>
2021-09-29 16:01:42 +0530

    uploaded requirement.text file

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'django_forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#          'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+#  }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd2eu7bnnf46rcr',
+        'USER': 'dthlioivjtqvrr',
+        'HOST': 'ec2-34-197-135-44.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'PASSWORD': 'a8c08ae3725f96448571c8e7d27987be5c5547ab8279e68641aaef14227cfafc',
     }
 }
 
